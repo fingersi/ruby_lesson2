8df1d2f58bf03fd58a654af10594fd439610a6e9
Алексей <alyoshinaa@gmail.com>
2020-12-07 18:30:46 +0300

    array from 10 to 100 with step 5

--- hash_and_array.rb
+++ hash_and_array.rb
@@ -1,5 +1,8 @@
 require 'date'
 
+# Сделать хеш, содержащий месяцы и количество дней в месяце. 
+# В цикле выводить те месяцы, у которых количество дней ровно 30
+
 days_in_months = [nil, 31, 28, 31, 30, 31, 30, 31,
 31, 30, 31, 30, 31]
 
@@ -15,3 +18,10 @@ hash_with_month.each{|month, days| thirty_days_only[month] = days if days == 30}
 
 puts thirty_days_only
 
+# Заполнить массив числами от 10 до 100 с шагом 5
+
+array_from = (10..100).step(5).to_a
+
+puts "Получившийся массив"
+
+array_from.each{|index| puts index}
