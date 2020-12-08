05a1466e51785258923e95296ba2b85daba11a21
Алексей <alyoshinaa@gmail.com>
2020-12-08 10:38:39 +0300

    date input remaked

--- date_calc.rb
+++ date_calc.rb
@@ -1,20 +1,36 @@
 
 puts 'Программа поиска порядкового номера даты'
-puts 'Введите дату в формате День:Месяц:Год'
+puts 'Введите дату в формате Год:Месяц:День'
 
 day = 0
 month = 0
 year = 0 
 
 loop do
-  puts 'Введите День. Введите целое число не равное нулю и меньшее 31.'
-  day_input = gets.chomp
-  if day_input.to_i > 0 && day_input.scan(/^-?[0-9]+$/).any? && day_input.to_i <= 31 && !day_input.scan(/[.]+/).any?
-    day = day_input.to_i
-    break
+  puts 'Введите Год. Введите целое число не равное нулю и меньшее 9999.'
+  year_input = gets.chomp
+  if year_input.to_i > 0 && year_input.scan(/^-?[0-9]+$/).any? && year_input.to_i <= 9999 && !year_input.scan(/[.]+/).any?
+    year = year_input.to_i
+    break   puts 'Вы ввели символы или ноль. Повторите вводит и введите целое число'
   else
     puts 'Вы ввели не целое число. Повторите вводит и введите целое число.'
   end
+end
+
+leap_year = false
+
+if year % 4 == 0  
+  if year % 100 == 0 && !(year % 400 == 0)
+    puts 'Вы ввели не высокосный год.'
+  elsif year % 100 == 0 && year % 400 == 0
+    puts ' Вы ввели высокосный год.' 
+    leap_year = true
+  else
+    puts 'Вы ввели высокосный год.'
+    leap_year = true
+  end
+else
+  puts 'Год не высокосный.'
 end
 
 loop do
@@ -28,16 +44,32 @@ loop do
   end
 end
 
+days_in_months = [nil, 31, 28, 31, 30, 31, 30, 31,
+  31, 30, 31, 30, 31]
+
+if month == 2 && leap_year
+  days_in_months[2] = 29
+end
+
+
 loop do
-  puts 'Введите Год. Введите целое число не равное нулю и меньшее 9999.'
-  year_input = gets.chomp
-  if year_input.to_i > 0 && year_input.scan(/^-?[0-9]+$/).any? && year_input.to_i <= 9999 && !year_input.scan(/[.]+/).any?
-    year = year_input.to_i
-    break   puts 'Вы ввели символы или ноль. Повторите вводит и введите целое число'
+  puts "Введите День. Введите целое число не равное нулю и меньшее или равное: #{days_in_months[month]}."
+  day_input = gets.chomp
+  day_limit = 0
+
+
+  if day_input.to_i > 0 && day_input.scan(/^-?[0-9]+$/).any? && day_input.to_i <= days_in_months[month] && !day_input.scan(/[.]+/).any?
+    day = day_input.to_i
+    break
   else
     puts 'Вы ввели не целое число. Повторите вводит и введите целое число.'
   end
 end
 
+
+
+
+
 puts "Вы ввели сдедующую дату: #{day}.#{month}.#{year}"
+
 
